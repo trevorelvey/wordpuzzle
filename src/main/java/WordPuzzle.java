c5e24c0a928fa0d75f23d704506fc79588b4e2ff
Trevor <trevor.elvey@gmail.com>
2016-02-12 10:22:30 -0800

    pass first integration test

--- src/main/java/WordPuzzle.java
+++ src/main/java/WordPuzzle.java
@@ -1,12 +1,30 @@
 import java.util.HashMap;
+import java.util.Map;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class WordPuzzle {
     public static void main(String[] args) {
+      String layout = "templates/layout.vtl";
 
-    }
+      get("/", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("template", "templates/home.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+      get("/puzzle", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/puzzle.vtl");
+
+      String input= request.queryParams("input");
+      String puzzle = puzzleMaker(input);
+      model.put("puzzle", puzzle);
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
 
   public static String puzzleMaker(String input) {
     String puzzle = "";
--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -1,22 +1,23 @@
 import org.fluentlenium.adapter.FluentTest;
-import org.junit.*;
+import org.junit.ClassRule;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.fluentlenium.core.filter.FilterConstructor.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class IntegrationTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
-
-  @Override
   public WebDriver getDefaultDriver() {
-    return webDriver;
+      return webDriver;
   }
 
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-
-  //Tests go here
+  @Test
+    public void rootTest() {
+      goTo("http://localhost:4567");
+      assertThat(pageSource()).contains("Puzzle Maker");
+    }
 }
